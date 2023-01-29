078d98f73a9aab7dcb8ae659508717e4420c7b66
Vladimir Bubenko <bawmc@mail.ru>
2023-01-29 17:50:12 +0700

    done task 10

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Домашнее задание 2 - C#
+
+// Задача 10. Программа, принимает трехзначное значение, и на выходе показывает вторую цифру этого числа
+
+int findsecond(int number)
+{
+    int num = (number/10)%10;
+    return num; 
+}
+Console.Write("Введите число: ");
+int newnumber = Convert.ToInt32(Console.ReadLine());
+int result = findsecond(newnumber);
+Console.WriteLine(result);
+
